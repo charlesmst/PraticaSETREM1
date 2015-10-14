5e855c409eba6ebcc8bc2d64a62c379a9f7754d4
gustavo-michels <ghmichels@gmail.com>
2015-10-14 20:20:52 -0300

    Revert "Estoque teste"
    This reverts commit fba73d47cbbd91704272f9a837a536840dc8b5d0.

--- src/GUIPratica/src/model/estoque/Estoque.java
+++ src/GUIPratica/src/model/estoque/Estoque.java
@@ -4,102 +4,33 @@ import java.util.Date;
 import java.util.List;
 
 public class Estoque {
-    private int id;
-    private EstoqueEntrada entrada;
-    private int lote;
-    private Item item;
-    private int quantidadeCompra;
-    private int quantidadeDisponivel;
-    private double valorUnitario;
-    private double valorUnidadeVenda;
-    private Date dataValidade;
 
-    private List movimentacoes;
+	private int id;
 
-    public void insert() {
-    }
+	private EstoqueEntrada entrada;
 
-    public void update() {
-    }
+	private int lote;
 
-    public int getId() {
-        return id;
-    }
+	private Item item;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	private int quantidadeCompra;
 
-    public EstoqueEntrada getEntrada() {
-        return entrada;
-    }
+	private int quantidadeDisponivel;
 
-    public void setEntrada(EstoqueEntrada entrada) {
-        this.entrada = entrada;
-    }
+	private double valorUnitario;
 
-    public int getLote() {
-        return lote;
-    }
+	private double valorUnidadeVenda;
 
-    public void setLote(int lote) {
-        this.lote = lote;
-    }
+	private Date dataValidade;
 
-    public Item getItem() {
-        return item;
-    }
+	private List movimentacoes;
 
-    public void setItem(Item item) {
-        this.item = item;
-    }
+	public void insert() {
 
-    public int getQuantidadeCompra() {
-        return quantidadeCompra;
-    }
+	}
 
-    public void setQuantidadeCompra(int quantidadeCompra) {
-        this.quantidadeCompra = quantidadeCompra;
-    }
+	public void update() {
 
-    public int getQuantidadeDisponivel() {
-        return quantidadeDisponivel;
-    }
-
-    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
-        this.quantidadeDisponivel = quantidadeDisponivel;
-    }
-
-    public double getValorUnitario() {
-        return valorUnitario;
-    }
-
-    public void setValorUnitario(double valorUnitario) {
-        this.valorUnitario = valorUnitario;
-    }
-
-    public double getValorUnidadeVenda() {
-        return valorUnidadeVenda;
-    }
-
-    public void setValorUnidadeVenda(double valorUnidadeVenda) {
-        this.valorUnidadeVenda = valorUnidadeVenda;
-    }
-
-    public Date getDataValidade() {
-        return dataValidade;
-    }
-
-    public void setDataValidade(Date dataValidade) {
-        this.dataValidade = dataValidade;
-    }
-
-    public List getMovimentacoes() {
-        return movimentacoes;
-    }
-
-    public void setMovimentacoes(List movimentacoes) {
-        this.movimentacoes = movimentacoes;
-    }
+	}
 
 }
